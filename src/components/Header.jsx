b406d3950d729c3eca3b83671e968e402590b463
tmba <ut42.nu@gmail.com>
2024-09-30 18:05:43 +0900

    ✨ ヘッダー部分を追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+import styles from "./Header.module.css";
+import { Button } from "@/components/ui/button";
+
 export const Header = () => {
   return (
-    <header>
-      <div>Header</div>
+    <header className={styles.header}>
+      <h3 className="text-2xl font-semibold">Portfolio</h3>
+      <nav>
+        <ul>
+          <Link href="#">
+            <Button variant="ghost">Home</Button>
+          </Link>
+          <Link href="#">
+            <Button variant="ghost">About</Button>
+          </Link>
+          <Link href="#">
+            <Button variant="ghost">Contact</Button>
+          </Link>
+        </ul>
+      </nav>
     </header>
   );
 };
