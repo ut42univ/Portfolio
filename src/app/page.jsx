aa895473c31b40898cb9163eb2d235e88fd0e968
tmba <ut42.nu@gmail.com>
2024-09-30 18:06:48 +0900

    💄 ページのマージンを指定

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
+import style from "./Home.module.css";
 import { Main } from "@/components/Main";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 
 const Home = () => {
   return (
-    <div>
+    <div className={style.Home}>
       <Header />
       <Main />
       <Footer />
