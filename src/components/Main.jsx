815dd24848487a65fb9d2b9a88025cb0f5a91e18
tmba <ut42.nu@gmail.com>
2024-10-01 10:22:29 +0900

    💄 内容を少し変更

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,27 @@
-import { Button } from "./ui/button";
+import styles from "./Main.module.css";
 
 export const Main = () => {
   return (
-    <div>
-      <Button>Click me</Button>
-      <div>Main</div>
-    </div>
+    <main className={styles.main}>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Hello, I'm Takuya. Hello, I'm Takuya. Hello, I'm Takuya. Hello, I'm
+        Takuya. Hello, I'm Takuya. Hello, I'm Takuya. Hello, I'm Takuya. Hello,
+        I'm Takuya. Hello, I'm Takuya. Hello, I'm Takuya. Hello, I'm Takuya.
+        Hello, I'm Takuya. Hello, I'm Takuya. Hello, I'm Takuya. Hello, I'm
+        Takuya. Hello, I'm Takuya. Hello, I'm Takuya. Hello, I'm Takuya. Hello,
+        I'm Takuya. Hello, I'm Takuya. Hello, I'm Takuya. Hello, I'm Takuya.
+        Hello, I'm Takuya. Hello, I'm Takuya. Hello, I'm Takuya. Hello, I'm
+        Takuya. Hello, I'm Takuya. Hello, I'm Takuya. Hello, I'm Takuya. Hello,
+        I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya.
+        I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya.
+        I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya.
+        I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya.
+        I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya.
+        I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya.
+        I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya.
+        I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya. I'm Takuya.
+        I'm Takuya.
+      </h1>
+    </main>
   );
 };
