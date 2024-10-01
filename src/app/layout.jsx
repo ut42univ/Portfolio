d399fd51b0811cd3c7cf4a7a36a4bdf5d1ffc29e
tmba <ut42.nu@gmail.com>
2024-10-01 19:14:52 +0900

    🐛  フォントの復元

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,17 @@
+import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata = {
   title: "Portfolio",
@@ -9,7 +21,7 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="ja" suppressHydrationWarning>
       <head />
-      <body>
+      <body className={(geistSans.variable, geistMono.variable)}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
