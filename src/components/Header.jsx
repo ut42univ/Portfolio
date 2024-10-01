9b5d352a3819d50e8c99c0daa37af2d2077d9667
tmba <ut42.nu@gmail.com>
2024-10-01 10:21:49 +0900

    ✨ ヘッダーの追加・モードのドロップダウンを追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,40 @@
 import Link from "next/link";
 import styles from "./Header.module.css";
 import { Button } from "@/components/ui/button";
+import { Github, Twitter } from "lucide-react";
+import { ModeToggle } from "@/components/ModeToggle";
+
+const urlTwitter = "https://twitter.com/ut42_dev";
+const urlGithub = "https://github.com/ut42univ";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <h3 className="text-2xl font-semibold">Portfolio</h3>
+      <Link href="/">
+        <h3 className="text-2xl font-semibold">UT's Portfolio</h3>
+      </Link>
       <nav>
         <ul>
-          <Link href="#">
+          <Link href="/">
             <Button variant="ghost">Home</Button>
           </Link>
-          <Link href="#">
+          <Link href="/about">
             <Button variant="ghost">About</Button>
           </Link>
-          <Link href="#">
-            <Button variant="ghost">Contact</Button>
+          <Link href="/news">
+            <Button variant="ghost">News</Button>
           </Link>
+          <Link href={urlTwitter} target="_blank">
+            <Button variant="ghost" size="icon">
+              <Twitter />
+            </Button>
+          </Link>
+          <Link href={urlGithub} target="_blank">
+            <Button variant="ghost" size="icon">
+              <Github />
+            </Button>
+          </Link>
+          <ModeToggle />
         </ul>
       </nav>
     </header>
---
+++ src/components/ModeToggle.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from "react";
+import { Moon, Sun } from "lucide-react";
+// import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export const ModeToggle = () => {
+  // モードの切り替えは未実装
+  // const { setTheme } = useTheme();
+  const setTheme = (theme) => {
+    console.log("setTheme", theme);
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
