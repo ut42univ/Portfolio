0874810d28c3debced87a97ec9aa6fc2a75c3d25
tmba <ut42.nu@gmail.com>
2024-10-03 13:47:10 +0900

    ♻️ 不要なCSSを削除し、Googleフォントに変更

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,13 @@
-import localFont from "next/font/local";
 import "./globals.css";
+import { Inter, JetBrains_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ["latin"],
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
 });
 
 export const metadata = {
@@ -21,7 +18,7 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="ja" suppressHydrationWarning>
       <head />
-      <body className={(geistSans.variable, geistMono.variable)}>
+      <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,9 @@
-import style from "./Home.module.css";
 import { Header } from "@/components/Header";
 import { Main } from "@/components/Main";
 
 const Home = () => {
   return (
-    <div className={style.Home}>
+    <div className="text-center my-8 mx-16 h-screen">
       <Header />
       <Main />
     </div>
