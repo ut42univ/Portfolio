bfb428a666c9d0f56c5a85c96e35620c7535d6c6
tmba <ut42.nu@gmail.com>
2024-10-18 17:17:43 +0900

    🐛 pulseのアニメーションを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,9 +65,18 @@ module.exports = {
           "background-position": "0% 50%",
         },
       },
+      pulse: {
+        "0%, 100%": {
+          opacity: 1,
+        },
+        "50%": {
+          opacity: 0.5,
+        },
+      },
     },
     animation: {
       gradient: "gradientMove 10s ease infinite",
+      pulse: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
     },
   },
   plugins: [require("tailwindcss-animate")],
