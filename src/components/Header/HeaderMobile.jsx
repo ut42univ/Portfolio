ae36b86e0d8fcc2b6b058d0849f993caf41d9a8d
tmba <ut42.nu@gmail.com>
2024-10-29 21:31:08 +0900

    🚚 アバターURLを変更

--- src/components/Header/HeaderMobile.jsx
+++ src/components/Header/HeaderMobile.jsx
@@ -9,13 +9,14 @@ import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export const HeaderMobile = () => {
+  const avatarSrc = "https://avatars.githubusercontent.com/u/119403405";
   const { isMenuOpen, toggleMenu } = useToggleMenu();
 
   return (
     <header className="fixed top-0 z-50 h-16 w-full px-4 mb-8 flex justify-between items-center bg-white bg-opacity-80 shadow-xl backdrop-blur-xl border-b-1 border-black border-opacity-10 dark:bg-black dark:bg-opacity-80 dark:border-white dark:border-opacity-10">
       <Link href="/" className="flex items-center gap-2">
         <Avatar className="border-2 h-8 w-8">
-          <AvatarImage src="https://pbs.twimg.com/profile_images/1835729229599932416/ZBg36eLt_400x400.jpg" />
+          <AvatarImage src={avatarSrc} />
           <AvatarFallback>UT</AvatarFallback>
         </Avatar>
         <h3 className="text-xl font-bold">
--- src/components/Header/HeaderPC.jsx
+++ src/components/Header/HeaderPC.jsx
@@ -4,11 +4,13 @@ import { ModeToggle } from "@/components/ModeToggle";
 import { NavPC } from "@/components/Header/NavPC";
 
 export const HeaderPC = () => {
+  const avatarSrc = "https://avatars.githubusercontent.com/u/119403405";
+
   return (
     <header className="fixed top-4 z-50 h-20 left-8 right-8 max-w-[64rem] py-4 px-12 mx-auto flex justify-between items-center bg-white bg-opacity-80 shadow-xl backdrop-blur-lg rounded-3xl border-2 border-black border-opacity-10 dark:bg-black dark:bg-opacity-80 dark:border-white dark:border-opacity-10">
       <Link href="/" className="flex gap-3 items-center">
         <Avatar className="border-2">
-          <AvatarImage src="https://pbs.twimg.com/profile_images/1835729229599932416/ZBg36eLt_400x400.jpg" />
+          <AvatarImage src={avatarSrc} />
           <AvatarFallback>UT</AvatarFallback>
         </Avatar>
         <h3 className="text-3xl font-extrabold">
